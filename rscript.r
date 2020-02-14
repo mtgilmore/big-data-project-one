baab36c400a6aecf641605b3f7eeaf72063c37da
mtgilmore <mtgilmore@gwu.edu>
2020-02-13 19:18:03 -0500

    Import data and begin plotting

--- rscript.r
+++ rscript.r
@@ -1,2 +1,10 @@
-print("Hello World")
-a = 5
+install.packages("igraph")
+install.packages("sna")
+library(igraph)
+library(sna)
+
+# Replace with your file path
+data_path = "C:/Users/mtgil/Desktop/big-data/big-data-project-one/email-EU/email-EU.edges"
+
+data = igraph::read.graph(data_path, directed=FALSE)
+adj_matrix = rgraph()
