c3cada22826123aa55fea594f294c59c716cac96
ericwendt <ericwendt@gwmail.gwu.edu>
2020-02-16 08:26:26 -0500

    added write up with 80% #4

--- rscript.r
+++ rscript.r
@@ -1,10 +1,55 @@
 install.packages("igraph")
 install.packages("sna")
+
 library(igraph)
 library(sna)
 
-# Replace with your file path
-data_path = "C:/Users/mtgil/Desktop/big-data/big-data-project-one/email-EU/email-EU.edges"
+g <- read.graph("C:/Users/Eric/Desktop/Spring_2020/BigData/project1/email-EU.edges", format="ncol", directed=F)
 
-data = igraph::read.graph(data_path, directed=FALSE)
-adj_matrix = rgraph()
+#g.adj <- rgraph("graph", FALSE)
+g.adj
+
+delete_edges(g, g)
+
+avg_dist = mean_distance(g, directed = FALSE, unconnected = TRUE)
+print(avg_dist)
+
+g<-igraph::graph_from_adjacency_matrix(g.adj, mode="undirected")
+
+simplify(g, remove.multiple = TRUE, remove.loops = TRUE, edge.attr.comb = igraph_opt("edge.attr.comb"))
+
+is_weighted(g)
+
+component_distribution(g, cumulative = FALSE, mul.size = FALSE)
+
+
+cliques(g, min = NULL, max = NULL)
+
+max_cliques(g, min = NULL, max = NULL, subset = NULL,
+          file = NULL)
+
+
+vertex_connectivity(g, source = 1, target = 5,
+                    checks = TRUE)
+vertex_connectivity(g, source = 5, target = 1,
+                    checks = TRUE)
+vertex_connectivity(g, source = 1, target = 4,
+                    checks = TRUE)
+vertex_connectivity(g, source = 5, target = 7,
+                    checks = TRUE)
+
+vertex_connectivity(g, source = 3, target = 7,
+                    checks = TRUE)
+diameter(g, directed = TRUE, unconnected = TRUE, weights = NULL)
+
+isomorphic(g, g, method = c("auto", "direct", "vf2", "bliss"))
+
+as_adj_list(g, mode = c("all", "out", "in", "total"))
+
+which_multiple(g, eids = E(g))
+
+knn(g, vids = V(g), weights = NULL)
+
+as_edgelist(g, names = TRUE)
+
+plot(g)
